ef612d5bddab3feafe397b5eefe1c67140088b2c
Grey <graybear150@gmail.com>
2024-03-31 19:38:10 -0400

    test

--- server.py
+++ server.py
@@ -1,19 +1,21 @@
-from flask import Flask, render_template, request, jsonify, send_from_directory
+from flask import Flask, request, send_from_directory, json
 from flask_cors import CORS
+from waitress import serve
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/<path:filename>')
 def serve_file(filename):
+    print(filename)
     # Serve files from the "public" directory based on the URL path
     return send_from_directory('public', filename)
 
 @app.route('/api', methods=['POST'])
 def api():
-    data = request.json
-    print('Received data:', data)
-    return jsonify({'message': 'Data received successfully'})
+    data = json.loads(request.data)
+    print(data)
+    return data
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    serve(app, host="0.0.0.0", port=80)

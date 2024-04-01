015618c7f936125b704b965c82451909d8992695
Grey <graybear150@gmail.com>
2024-03-31 20:02:15 -0400

    test

--- server.py
+++ server.py
@@ -1,20 +1,21 @@
 from flask import Flask, request, send_from_directory, json
 from flask_cors import CORS
 from waitress import serve
+import logging
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/<path:filename>')
 def serve_file(filename):
-    print(filename)
+    app.logger.info(filename)
     # Serve files from the "public" directory based on the URL path
     return send_from_directory('public', filename)
 
 @app.route('/api', methods=['POST'])
 def api():
-    data = json.loads(request.data)
-    print(data)
+    data = json.loads(request. data)
+    app.logger.info(data, flush=True)
     return data
 
 if __name__ == '__main__':
